e873f516152a64dee3665c5d3d6c6ebf07a038d0
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-09 20:36:45 +0100

    Update nop_sub.c

--- nop_sub.c
+++ nop_sub.c
@@ -21,7 +21,6 @@ void s_nop(stack_t **stack, unsigned int line_number)
  */
 void s_sub(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp1;
 	stack_t *temp2;
 	int diff = 0;
 
@@ -29,16 +28,14 @@ void s_sub(stack_t **stack, unsigned int line_number)
 	{
 		temp2 = (*stack)->next;
 		diff = temp2->n - (*stack)->n;
-		temp1 = *stack;
 		*stack = (*stack)->next;
 		if (*stack)
 			(*stack)->prev = NULL;
-		free(temp1);
 		(*stack)->n = diff;
 	}
 	else
 	{
-		fprintf(stderr, "L%u can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		free(global.content);
 		fclose(global.file);
 		free_stack(stack);
